009b17ea9e917629b1ff3af37e9c6f1f3060e702
hubertpie <hubert.pietron95@gmail.com>
2020-12-16 11:01:03 +0100

    added views, templates, urls for CRUD course

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -20,7 +20,7 @@ class Course(models.Model):
     subject =models.ForeignKey(Subject, related_name='courses', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
-    overView = models.TextField()
+    overview = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
