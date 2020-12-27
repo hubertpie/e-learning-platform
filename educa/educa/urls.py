f511d33d473d66d85f8589a9d973b69bca5c0e7f
hubertpie <hubert.pietron95@gmail.com>
2020-12-27 07:01:49 +0100

    created students app, added enroll button to course_detail_view

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('course/', include('courses.urls')),
+    path('students/', include('students.urls')),
     path('', CourseListView.as_view(), name='course_list'),
 ]
